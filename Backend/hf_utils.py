ddf9b15b3be58d65ee5912af7d052f1b6e30eeee
rajtharani77 <117341499+rajtharani77@users.noreply.github.com>
2025-07-17 20:53:20 +0530

    Update hf_utils.py

--- Backend/hf_utils.py
+++ Backend/hf_utils.py
@@ -1,15 +1,21 @@
-# Backend/hf_utils.py
+# Backend/api_utils.py
 import os
+import streamlit as st
 
 def get_hf_token() -> str:
-    """
-    Load HUGGINGFACE_API_TOKEN from Streamlit secrets or environment.
-    """
     try:
-        from streamlit import secrets
-        return secrets["HUGGINGFACE_API_TOKEN"]
-    except Exception:
+        return st.secrets["HUGGINGFACE_API_TOKEN"]
+    except:
         token = os.getenv("HUGGINGFACE_API_TOKEN")
         if not token:
-            raise RuntimeError("HUGGINGFACE_API_TOKEN not found in st.secrets or ENV")
+            raise RuntimeError("Set HUGGINGFACE_API_TOKEN in env or st.secrets")
+        return token
+
+def get_assemblyai_token() -> str:
+    try:
+        return st.secrets["ASSEMBLYAI_API_TOKEN"]
+    except:
+        token = os.getenv("ASSEMBLYAI_API_TOKEN")
+        if not token:
+            raise RuntimeError("Set ASSEMBLYAI_API_TOKEN in env or st.secrets")
         return token
